b25a8ccd104d684a3184bc8fe3e69b2976cdbb15
krisnayo18 <krisnayoga18@gmail.com>
2019-05-05 16:08:29 +0700

    update jurnal

--- SIA/ClassLibraryJurnal/Jurnal.cs
+++ SIA/ClassLibraryJurnal/Jurnal.cs
@@ -11,13 +11,13 @@ namespace SIA_LIB
         private Transaksi transaksi;
         private List<DetilJurnal> listDetilJurnal;
 
-        public string IdJurnal { get => idJurnal; set => idJurnal = value; }
-        public string Keterangan { get => keterangan; set => keterangan = value; }
-        public string NomorBukti { get => nomorBukti; set => nomorBukti = value; }
-        public string Jenis { get => jenis; set => jenis = value; }
-        public DateTime Tanggal { get => tanggal; set => tanggal = value; }
-        internal Periode Periode { get => periode; set => periode = value; }
-        internal Transaksi Transaksi { get => transaksi; set => transaksi = value; }
-        internal List<DetilJurnal> ListDetilJurnal { get => listDetilJurnal; set => listDetilJurnal = value; }
+        //public string IdJurnal { get => idJurnal; set => idJurnal = value; }
+        //public string Keterangan { get => keterangan; set => keterangan = value; }
+        //public string NomorBukti { get => nomorBukti; set => nomorBukti = value; }
+        //public string Jenis { get => jenis; set => jenis = value; }
+        //public DateTime Tanggal { get => tanggal; set => tanggal = value; }
+        //internal Periode Periode { get => periode; set => periode = value; }
+        //internal Transaksi Transaksi { get => transaksi; set => transaksi = value; }
+        //internal List<DetilJurnal> ListDetilJurnal { get => listDetilJurnal; set => listDetilJurnal = value; }
     }
 }
